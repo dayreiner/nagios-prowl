0405b67836c242eb00e0968aae3d0929062f53e0
Jeff Walter <jwalter@opendns.com>
2012-10-11 15:57:52 -0700

    And here it is.

---
+++ Readme.md
@@ -0,0 +1,45 @@
+# Nagios-Prowl
+
+## Description
+
+Allows you to send cleanly formatted Prowl notifications for Nagios alerts.
+
+## Requirements
+
+* `prowl.pl` from http://prowl.weks.net/static/prowl.pl (included in this repo)
+* Perl modules
+** LWP::UserAgent
+** Crypt::SSLeay
+* bash
+** grep
+** paste
+
+# Use
+
+* Grab `nagios-prowl.sh` and `prowl.pl`.  Put them somewhere nice.
+* Open `nagios-prowl.sh` in your favorite editor:
+  * Change `NAGIOSNAME` to something that will identify messages from this source.
+  * Change `PROWL_PROVIDERKEY` to the provider key you created at http://prowlapp.com/
+  * Change `URLBASE` to the URL for the Nagios cgi-bin. Something like "http://localhost/nagios/cgi-bin".
+  * Change `PROWLPLPATH` to the absolute path for `prowl.pl`
+* Set mode for `nagios-prowl.sh` and `prowl.pl` to 0555, or something more restrictive
+* In Nagios:
+  * Add the following two commands:
+
+    define command {
+        command_name notify-host-by-prowl
+        command_line /opt/ops/nagios-prowl.sh "$LONGDATETIME$" "Host" "$NOTIFICATIONTYPE$" "$HOSTSTATE$" "$HOSTNAME$" "$HOSTDESC$" "$HOSTOUTPUT$" -- $_CONTACTPROWL_APIKEYS$
+    }
+    
+    define command {
+        command_name notify-service-by-prowl
+        command_line /opt/ops/nagios-prowl.sh "$LONGDATETIME$" "Service" "$NOTIFICATIONTYPE$" "$SERVICESTATE$" "$HOSTNAME$/$SERVICEDESC$" "$SERVICEDESC$" "$SERVICEOUTPUT$" -- $_CONTACTPROWL_APIKEYS$
+    }
+
+  * Add the following to contact records you'd like Prowl notifications for:
+
+    define contact {
+         service_notification_commands <WHAT_YOU_HAD_BEFORE>,notify-service-by-prowl
+         host_notification_commands <WHAT_YOU_HAD_BEFORE>,notify-host-by-prowl
+        _prowl_apikeys <KEY>[, <KEY>...]
+    }
---
+++ nagios-prowl.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+NAGIOSNAME="Nagios"
+PROWL_PROVIDERKEY="<GET_THIS_KEY_FROM_PROWLAPP.COM>"
+URLBASE="https://localhost/nagios/cgi-bin"
+PROWLPLPATH="/usr/local/sbin/prowl.pl"
+
+# End of configuration options
+
+DATETIME="${1}" # Date/time of alert
+TYPE="${2}" # Host or Service
+NOTIFICATION="${3}" # PROBLEM, RESOLVED, etc
+STATE="${4}" # OK, WARNING, CRITICAL, UP, DOWN, UNKNOWN
+NAME="${5}" # Hostname or Hostname/Service
+DESC="${6}" # Host or service description
+OUTPUT="${7}" # Check command output
+
+shift 7 # Shift all of the required arguments off
+
+# Check we have everything and that the expected "--" is there
+if [ -z "${DATETIME}" ] && [ -z "${TYPE}" ] && [ -z "${NOTIFICATION}" ] && [ -z "${STATE}" ] && [ -z "${NAME}" ] && [ "${1}" != "--" ]; then
+	echo "Missing all args" >&2
+	exit 1
+fi
+
+shift # Get rid of the "--"
+
+# Set the priority level based on the host/service state
+PRIORITY="0"
+case "${STATE}" in
+	[oO][kK])
+		PRIORITY="-1" ;;
+	[uU][pP])
+		PRIORITY="-1" ;;
+	[wW][aA][rR][nN][iI][nN][gG])
+		PRIORITY="1" ;;
+	[cC][rR][iI][tT][iI][cC][aA][lL])
+		PRIORITY="2" ;;
+	[dD][oO][wW][nN])
+		PRIORITY="2" ;;
+	[uU][nN][kK][nN][oO][wW][nN])
+		PRIORITY="0" ;;
+esac
+
+# Set the URL based on whether this a host or service alert
+URL=""
+case "${TYPE}" in
+	[hH][oO][sS][tT])
+		URL="${URLBASE}/status.cgi?host=${NAME}" ;;
+	[sS][eE][rR][vV][iI][cC][eE])
+		URL="${URLBASE}/extinfo.cgi?type=2&host=$(echo "${NAME}" | cut -d / -f 1)&service=$(echo "${NAME}" | cut -d / -f 2 | sed -e 's/ /%20/g')" ;;
+esac
+
+# Build a string of comma-separated (no spaces) Prowl API keys
+APIKEYS=""
+while [ -n "${1}" ]; do
+	APIKEYS="${APIKEYS},$(echo "${1}" | grep -o '[a-f0-9][a-f0-9]*' | paste -s -d "")"
+	shift
+done
+APIKEYS="$(echo "${APIKEYS}" | sed -e 's/^,//')"
+
+# Make the prowl.pl call
+${PROWLPLPATH} -apikey="${APIKEYS}" -providerkey="${PROWL_PROVIDERKEY}" -application="${NAGIOSNAME}" -priority="${PRIORITY}" -event="${NAME} is ${STATE}" -notification="Date/Time: ${DATETIME} | Description: ${DESC} | Output: ${OUTPUT}" -url="${URL}"
